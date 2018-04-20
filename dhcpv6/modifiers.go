9d16ae7ccf445ef081712f1f1169d1f9163226b6
Andrea Barberio <insomniac@slackware.it>
2018-04-20 19:20:37 +0000

    dhcpv6: option userclass: supporting multiple user classes

--- dhcpv6/modifiers.go
+++ dhcpv6/modifiers.go
@@ -26,8 +26,9 @@ func WithNetboot(d DHCPv6) DHCPv6 {
 
 // WithUserClass adds a user class option to the packet
 func WithUserClass(uc []byte) Modifier {
+    // TODO let the user specify multiple user classes
 	return func(d DHCPv6) DHCPv6 {
-		ouc := OptUserClass{UserClass: uc}
+		ouc := OptUserClass{UserClasses: [][]byte{uc}}
 		d.AddOption(&ouc)
 		return d
 	}
