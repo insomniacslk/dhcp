4b1ce9484b69f4a9ba51d72963f60f4cd80e9918
Andrea Barberio <insomniac@slackware.it>
2018-04-26 09:41:12 +0000

    more tests

--- dhcpv6/option_userclass_test.go
+++ dhcpv6/option_userclass_test.go
@@ -1,7 +1,6 @@
 package dhcpv6
 
 import (
-	"log"
 	"testing"
 
 	"github.com/stretchr/testify/require"
@@ -13,7 +12,38 @@ func TestParseOptUserClass(t *testing.T) {
 	}
 	opt, err := ParseOptUserClass(expected)
 	require.Nil(t, err)
-	log.Printf("%+v", opt)
 	require.Equal(t, len(opt.UserClasses), 1)
 	require.Equal(t, opt.UserClasses[0], []byte("linuxboot"))
+}
+
+func TestParseOptUserClassMultiple(t *testing.T) {
+	expected := []byte{
+		0, 9, 'l', 'i', 'n', 'u', 'x', 'b', 'o', 'o', 't',
+		0, 4, 't', 'e', 's', 't',
+	}
+	opt, err := ParseOptUserClass(expected)
+	require.Nil(t, err)
+	require.Equal(t, len(opt.UserClasses), 2)
+	require.Equal(t, opt.UserClasses[0], []byte("linuxboot"))
+	require.Equal(t, opt.UserClasses[1], []byte("test"))
+}
+
+func TestParseOptUserClassNone(t *testing.T) {
+	expected := []byte{}
+	opt, err := ParseOptUserClass(expected)
+	require.Nil(t, err)
+	require.Equal(t, len(opt.UserClasses), 0)
+}
+
+func TestOptUserClassToBytes(t *testing.T) {
+	opt := OptUserClass{
+		UserClasses: [][]byte{[]byte("linuxboot")},
+	}
+	data := opt.ToBytes()
+	expected := []byte{
+		0, 15, // OPTION_USER_CLASS
+		0, 11, // length
+		0, 9, 'l', 'i', 'n', 'u', 'x', 'b', 'o', 'o', 't',
+	}
+	require.Equal(t, data, expected)
 }
