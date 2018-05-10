eab1c08cbc637f5d370bf84da1feba8673fb7cc3
insomniac <insomniacslk@users.noreply.github.com>
2018-05-10 11:52:34 +0200

    OptDNSRecursiveNameServer gets public fields and tests, removes setter and getter (#62)

--- dhcpv6/option_dnsrecursivenameserver.go
+++ dhcpv6/option_dnsrecursivenameserver.go
@@ -9,51 +9,49 @@ import (
 	"net"
 )
 
+// OptDNSRecursiveNameServer represents a DNS_RECURSIVE_NAME_SERVER option
 type OptDNSRecursiveNameServer struct {
-	nameServers []net.IP
+	NameServers []net.IP
 }
 
+// Code returns the option code
 func (op *OptDNSRecursiveNameServer) Code() OptionCode {
 	return DNS_RECURSIVE_NAME_SERVER
 }
 
+// ToBytes returns the option serialized to bytes, including option code and
+// length
 func (op *OptDNSRecursiveNameServer) ToBytes() []byte {
 	buf := make([]byte, 4)
 	binary.BigEndian.PutUint16(buf[0:2], uint16(DNS_RECURSIVE_NAME_SERVER))
 	binary.BigEndian.PutUint16(buf[2:4], uint16(op.Length()))
-	for _, ns := range op.nameServers {
+	for _, ns := range op.NameServers {
 		buf = append(buf, ns...)
 	}
 	return buf
 }
 
-func (op *OptDNSRecursiveNameServer) NameServers() []net.IP {
-	return op.nameServers
-}
-
-func (op *OptDNSRecursiveNameServer) SetNameServers(ns []net.IP) {
-	op.nameServers = ns
-}
-
+// Length returns the option length
 func (op *OptDNSRecursiveNameServer) Length() int {
-	return len(op.nameServers) * net.IPv6len
+	return len(op.NameServers) * net.IPv6len
 }
 
 func (op *OptDNSRecursiveNameServer) String() string {
-	return fmt.Sprintf("OptDNSRecursiveNameServer{nameservers=%v}", op.nameServers)
+	return fmt.Sprintf("OptDNSRecursiveNameServer{nameservers=%v}", op.NameServers)
 }
 
-// build an OptDNSRecursiveNameServer structure from a sequence of bytes.
-// The input data does not include option code and length bytes.
+// ParseOptDNSRecursiveNameServer builds an OptDNSRecursiveNameServer structure
+// from a sequence of bytes. The input data does not include option code and length
+// bytes.
 func ParseOptDNSRecursiveNameServer(data []byte) (*OptDNSRecursiveNameServer, error) {
-	if len(data)%2 != 0 {
-		return nil, fmt.Errorf("Invalid OptDNSRecursiveNameServer data: length is not a multiple of 2")
+	if len(data)%net.IPv6len != 0 {
+		return nil, fmt.Errorf("Invalid OptDNSRecursiveNameServer data: length is not a multiple of %d", net.IPv6len)
 	}
 	opt := OptDNSRecursiveNameServer{}
 	var nameServers []net.IP
 	for i := 0; i < len(data); i += net.IPv6len {
 		nameServers = append(nameServers, data[i:i+net.IPv6len])
 	}
-	opt.nameServers = nameServers
+	opt.NameServers = nameServers
 	return &opt, nil
 }
