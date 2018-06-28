2fbc46235fea000dca15ae1a50f96fccf5895941
Andrea Barberio <insomniac@slackware.it>
2018-06-28 14:05:14 +0100

    Added tests for OptionDomainNameServer

---
+++ dhcpv4/option_domain_name_server_test.go
@@ -0,0 +1,55 @@
+package dhcpv4
+
+import (
+	"net"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestOptDomainNameServerInterfaceMethods(t *testing.T) {
+	servers := []net.IP{
+		net.IPv4(192, 168, 0, 10),
+		net.IPv4(192, 168, 0, 20),
+	}
+	o := OptDomainNameServer{NameServers: servers}
+	require.Equal(t, OptionDomainNameServer, o.Code(), "Code")
+	require.Equal(t, net.IPv4len*len(servers), o.Length(), "Length")
+	require.Equal(t, servers, o.NameServers, "NameServers")
+}
+
+func TestParseOptDomainNameServer(t *testing.T) {
+	data := []byte{
+		byte(OptionDomainNameServer),
+		8,               // Length
+		192, 168, 0, 10, // DNS #1
+		192, 168, 0, 20, // DNS #2
+	}
+	o, err := ParseOptDomainNameServer(data)
+	require.NoError(t, err)
+	servers := []net.IP{
+		net.IPv4(192, 168, 0, 10),
+		net.IPv4(192, 168, 0, 20),
+	}
+	require.Equal(t, &OptDomainNameServer{NameServers: servers}, o)
+
+	// Short byte stream
+	data = []byte{byte(OptionDomainNameServer)}
+	_, err = ParseOptDomainNameServer(data)
+	require.Error(t, err, "should get error from short byte stream")
+
+	// Wrong code
+	data = []byte{54, 2, 1, 1}
+	_, err = ParseOptDomainNameServer(data)
+	require.Error(t, err, "should get error from wrong code")
+
+	// Bad length
+	data = []byte{byte(OptionDomainNameServer), 6, 1, 1, 1}
+	_, err = ParseOptDomainNameServer(data)
+	require.Error(t, err, "should get error from bad length")
+}
+
+func TestOptDomainNameServerString(t *testing.T) {
+	o := OptDomainNameServer{NameServers: []net.IP{net.IPv4(192, 168, 0, 1), net.IPv4(192, 168, 0, 10)}}
+	require.Equal(t, "Domain Name Servers -> 192.168.0.1, 192.168.0.10", o.String())
+}
