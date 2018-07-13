2b18d4aca0504a12a0b2a24e0dbec12f87c12241
Andrea Barberio <insomniac@slackware.it>
2018-07-13 16:11:24 +0100

    [OptIANA] Added GetOneOption method

--- dhcpv6/option_nontemporaryaddress.go
+++ dhcpv6/option_nontemporaryaddress.go
@@ -45,6 +45,10 @@ func (op *OptIANA) String() string {
 		op.IaId, op.T1, op.T2, op.Options)
 }
 
+func (op *OptIANA) GetOneOption(code OptionCode) Option {
+	return getOption(op.Options, code)
+}
+
 // DelOption will remove all the options that match a Option code.
 func (op *OptIANA) DelOption(code OptionCode) {
 	op.Options = delOption(op.Options, code)
