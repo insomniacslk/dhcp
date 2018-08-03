cff664d4ac7fe61377195a9d439e17259ce5899e
Pablo Mazzini <pmazzini@gmail.com>
2018-08-03 12:23:39 +0200

    IsRequested: update comment

--- dhcpv4/utils.go
+++ dhcpv4/utils.go
@@ -1,7 +1,7 @@
 package dhcpv4
 
 // IsRequested function takes a DHCPv4 message and an OptionCode, and returns
-// true if that option is within the requested options of the DHCPv6 message.
+// true if that option is within the requested options of the DHCPv4 message.
 func IsRequested(pkt *DHCPv4, requested OptionCode) bool {
 	for _, optprl := range pkt.GetOption(OptionParameterRequestList) {
 		for _, o := range optprl.(*OptParameterRequestList).RequestedOpts {
