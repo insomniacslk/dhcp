5207d76712250f33111c546d9ace98336d616bfc
Owen Mooney <mooneyow@tcd.ie>
2018-08-10 13:33:46 +0100

    Added modifier for netbootv4 (#124)

--- dhcpv4/modifiers.go
+++ dhcpv4/modifiers.go
@@ -1,11 +1,49 @@
 package dhcpv4
 
-// WithUserClass adds a user class option to the packet
-func WithUserClass(uc []byte) Modifier {
+// WithUserClass adds a user class option to the packet.
+// The rfc parameter allows you to specify if the userclass should be
+// rfc compliant or not. More details in issue #113
+func WithUserClass(uc []byte, rfc bool) Modifier {
 	// TODO let the user specify multiple user classes
 	return func(d *DHCPv4) *DHCPv4 {
-		ouc := OptUserClass{UserClasses: [][]byte{uc}}
+		ouc := OptUserClass{
+			UserClasses: [][]byte{uc},
+			Rfc3004:     rfc,
+		}
 		d.AddOption(&ouc)
 		return d
 	}
+}
+
+// WithNetboot adds bootfile URL and bootfile param options to a DHCPv4 packet.
+func WithNetboot(d *DHCPv4) *DHCPv4 {
+	params := d.GetOneOption(OptionParameterRequestList)
+
+	var (
+		OptParams                 *OptParameterRequestList
+		foundOptionTFTPServerName bool
+		foundOptionBootfileName   bool
+	)
+	if params != nil {
+		OptParams = params.(*OptParameterRequestList)
+		for _, option := range OptParams.RequestedOpts {
+			if option == OptionTFTPServerName {
+				foundOptionTFTPServerName = true
+			} else if option == OptionBootfileName {
+				foundOptionBootfileName = true
+			}
+		}
+		if !foundOptionTFTPServerName {
+			OptParams.RequestedOpts = append(OptParams.RequestedOpts, OptionTFTPServerName)
+		}
+		if !foundOptionBootfileName {
+			OptParams.RequestedOpts = append(OptParams.RequestedOpts, OptionBootfileName)
+		}
+	} else {
+		OptParams = &OptParameterRequestList{
+			RequestedOpts: []OptionCode{OptionTFTPServerName, OptionBootfileName},
+		}
+		d.AddOption(OptParams)
+	}
+	return d
 }
--- dhcpv4/modifiers_test.go
+++ dhcpv4/modifiers_test.go
@@ -8,7 +8,62 @@ import (
 
 func TestUserClassModifier(t *testing.T) {
 	d, _ := New()
-	userClass := WithUserClass([]byte("linuxboot"))
+	userClass := WithUserClass([]byte("linuxboot"), false)
 	d = userClass(d)
+	expected := []byte{
+		77, // OptionUserClass
+		9,  // length
+		'l', 'i', 'n', 'u', 'x', 'b', 'o', 'o', 't',
+	}
 	require.Equal(t, "User Class Information -> linuxboot", d.options[0].String())
+	require.Equal(t, expected, d.options[0].ToBytes())
+}
+
+func TestUserClassModifierRFC(t *testing.T) {
+	d, _ := New()
+	userClass := WithUserClass([]byte("linuxboot"), true)
+	d = userClass(d)
+	expected := []byte{
+		77, // OptionUserClass
+		10, // length
+		9, 'l', 'i', 'n', 'u', 'x', 'b', 'o', 'o', 't',
+	}
+	require.Equal(t, "User Class Information -> linuxboot", d.options[0].String())
+	require.Equal(t, expected, d.options[0].ToBytes())
+}
+
+func TestWithNetboot(t *testing.T) {
+	d, _ := New()
+	d = WithNetboot(d)
+	require.Equal(t, "Parameter Request List -> [TFTP Server Name, Bootfile Name]", d.options[0].String())
+}
+
+func TestWithNetbootExistingTFTP(t *testing.T) {
+	d, _ := New()
+	OptParams := &OptParameterRequestList{
+		RequestedOpts: []OptionCode{OptionTFTPServerName},
+	}
+	d.AddOption(OptParams)
+	d = WithNetboot(d)
+	require.Equal(t, "Parameter Request List -> [TFTP Server Name, Bootfile Name]", d.options[0].String())
+}
+
+func TestWithNetbootExistingBootfileName(t *testing.T) {
+	d, _ := New()
+	OptParams := &OptParameterRequestList{
+		RequestedOpts: []OptionCode{OptionBootfileName},
+	}
+	d.AddOption(OptParams)
+	d = WithNetboot(d)
+	require.Equal(t, "Parameter Request List -> [Bootfile Name, TFTP Server Name]", d.options[0].String())
+}
+
+func TestWithNetbootExistingBoth(t *testing.T) {
+	d, _ := New()
+	OptParams := &OptParameterRequestList{
+		RequestedOpts: []OptionCode{OptionBootfileName, OptionTFTPServerName},
+	}
+	d.AddOption(OptParams)
+	d = WithNetboot(d)
+	require.Equal(t, "Parameter Request List -> [Bootfile Name, TFTP Server Name]", d.options[0].String())
 }
