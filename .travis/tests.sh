1d55b46a34be2b6776c438b5425a3e0afb9d9905
Andrea Barberio <insomniac@slackware.it>
2018-09-27 00:28:48 +0100

    Avoid router7 check on go1.9

--- .travis/tests.sh
+++ .travis/tests.sh
@@ -16,6 +16,14 @@ done
 
 # check that we are not breaking some projects that depend on us. Remove this after moving to
 # Go versioned modules, see https://github.com/insomniacslk/dhcp/issues/123
+
+# Skip go1.9 for this check. rtr7/router7 depends on miekg/dns, which does not
+# support go1.9
+if [ "$TRAVIS_GO_VERSION" = "1.9" ]
+then
+    exit 0
+fi
+
 go get github.com/rtr7/router7/cmd/...
 cd "${GOPATH}/src/github.com/rtr7/router7"
 go build github.com/rtr7/router7/cmd/...
