22ac48b1f0dc3ca8a4792e8f9ff54f6e4f70c0cf
Andrea Barberio <insomniac@slackware.it>
2018-11-06 00:56:00 +0000

    Third attempt

--- .travis/tests.sh
+++ .travis/tests.sh
@@ -14,7 +14,7 @@ for d in $(go list ./... | grep -v vendor); do
     fi
     # integration tests
     go test -c -tags=integration -race -coverprofile=profile.out -covermode=atomic $d
-    sudo "./$d/$(basename $d).test"
+    sudo "./$(basename $d).test"
     if [ -f profile.out ]; then
         cat profile.out >> coverage.txt
         rm profile.out
