bc78ce288a82487b8c03c3f606e1dec32d287891
Andrea Barberio <insomniac@slackware.it>
2018-11-18 23:14:04 +0000

    Switched to AF_PACKET

--- dhcpv4/client.go
+++ dhcpv4/client.go
@@ -126,24 +126,15 @@ func MakeListeningSocket(ifname string) (int, error) {
 }
 
 func makeListeningSocketWithCustomPort(ifname string, port int) (int, error) {
-	fd, err := unix.Socket(unix.AF_INET, unix.SOCK_DGRAM, unix.IPPROTO_UDP)
+	fd, err := unix.Socket(unix.AF_PACKET, unix.SOCK_RAW, unix.ETH_P_ALL)
 	if err != nil {
 		return fd, err
 	}
-	err = unix.SetsockoptInt(fd, unix.SOL_SOCKET, unix.SO_REUSEADDR, 1)
-	if err != nil {
+	if err := BindToInterface(fd, ifname); err != nil {
 		return fd, err
 	}
-	var addr [4]byte
-	copy(addr[:], net.IPv4zero.To4())
-	if err = unix.Bind(fd, &unix.SockaddrInet4{Port: port, Addr: addr}); err != nil {
-		return fd, err
-	}
-	err = BindToInterface(fd, ifname)
-	if err != nil {
-		return fd, err
-	}
-	return fd, nil
+	err = unix.SetsockoptInt(fd, unix.SOL_SOCKET, unix.SO_BROADCAST, 1)
+	return fd, err
 }
 
 func toUDPAddr(addr net.Addr, defaultAddr *net.UDPAddr) (*net.UDPAddr, error) {
@@ -271,28 +262,30 @@ func (c *Client) sendReceive(sendFd, recvFd int, packet *DHCPv4, messageType Mes
 	// Create a goroutine to perform the blocking send, and time it out after
 	// a certain amount of time.
 	var (
-		destination [4]byte
+		destination [net.IPv4len]byte
 		response    *DHCPv4
 	)
 	copy(destination[:], raddr.IP.To4())
 	remoteAddr := unix.SockaddrInet4{Port: laddr.Port, Addr: destination}
 	recvErrors := make(chan error, 1)
 	go func(errs chan<- error) {
-		conn, innerErr := net.FileConn(os.NewFile(uintptr(recvFd), ""))
-		if innerErr != nil {
+		// set read timeout
+		timeout := unix.Timeval{
+			Sec:  int64(c.ReadTimeout.Seconds()),
+			Usec: 0,
+		}
+		if innerErr := unix.SetsockoptTimeval(recvFd, unix.SOL_SOCKET, unix.SO_RCVTIMEO, &timeout); innerErr != nil {
 			errs <- innerErr
 			return
 		}
-		defer conn.Close()
-		conn.SetReadDeadline(time.Now().Add(c.ReadTimeout))
-
 		for {
 			buf := make([]byte, MaxUDPReceivedPacketSize)
-			n, _, _, _, innerErr := conn.(*net.UDPConn).ReadMsgUDP(buf, []byte{})
+			n, innerErr := unix.Read(recvFd, buf)
 			if innerErr != nil {
 				errs <- innerErr
 				return
 			}
+			log.Print("SOMETHING")
 
 			response, innerErr = FromBytes(buf[:n])
 			if innerErr != nil {
@@ -319,12 +312,29 @@ func (c *Client) sendReceive(sendFd, recvFd int, packet *DHCPv4, messageType Mes
 		}
 		recvErrors <- nil
 	}(recvErrors)
+
+	// send the request while the goroutine waits for replies
 	if err = unix.Sendto(sendFd, packetBytes, 0, &remoteAddr); err != nil {
 		return nil, err
 	}
 
+	defer func() {
+		// close the sockets
+		if err := unix.Close(sendFd); err != nil {
+			log.Printf("unix.Close(sendFd) failed: %v", err)
+		}
+		if sendFd != recvFd {
+			if err := unix.Close(recvFd); err != nil {
+				log.Printf("unix.Close(recvFd) failed: %v", err)
+			}
+		}
+	}()
+
 	select {
 	case err = <-recvErrors:
+		if err == unix.EAGAIN {
+			return nil, errors.New("timed out while listening for replies")
+		}
 		if err != nil {
 			return nil, err
 		}
