cea6f8e6352294799af7236732a871eb1bc51511
Chris Koch <chrisko@google.com>
2019-09-24 20:50:35 -0700

    dhcpv6: simplify boot file URL
    Signed-off-by: Chris Koch <chrisko@google.com>

--- dhcpv6/option_bootfileurl_test.go
+++ dhcpv6/option_bootfileurl_test.go
@@ -8,24 +8,22 @@ import (
 )
 
 func TestOptBootFileURL(t *testing.T) {
-	expected := []byte("https://insomniac.slackware.it")
-	opt, err := ParseOptBootFileURL(expected)
+	expected := "https://insomniac.slackware.it"
+	opt, err := ParseOptBootFileURL([]byte(expected))
 	if err != nil {
 		t.Fatal(err)
 	}
-	if url := opt.BootFileURL; !bytes.Equal(url, expected) {
-		t.Fatalf("Invalid boot file URL. Expected %v, got %v", expected, url)
+	if string(opt) != expected {
+		t.Fatalf("Invalid boot file URL. Expected %v, got %v", expected, opt)
 	}
-	require.Contains(t, opt.String(), "BootFileUrl=https://insomniac.slackware.it", "String() should contain the correct BootFileUrl output")
+	require.Contains(t, opt.String(), "https://insomniac.slackware.it", "String() should contain the correct BootFileUrl output")
 }
 
 func TestOptBootFileURLToBytes(t *testing.T) {
-	urlString := []byte("https://insomniac.slackware.it")
-	opt := OptBootFileURL{
-		BootFileURL: urlString,
-	}
+	urlString := "https://insomniac.slackware.it"
+	opt := OptBootFileURL(urlString)
 	toBytes := opt.ToBytes()
-	if !bytes.Equal(toBytes, urlString) {
+	if !bytes.Equal(toBytes, []byte(urlString)) {
 		t.Fatalf("Invalid ToBytes result. Expected %v, got %v", urlString, toBytes)
 	}
 }
