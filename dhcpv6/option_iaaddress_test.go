69af707a4b1b3405bc3ed7619efb1f0aa3abf377
Chris Koch <chrisko@google.com>
2019-12-28 06:30:10 -0800

    v6: use time.Duration for duration fields
    Signed-off-by: Chris Koch <chrisko@google.com>

--- dhcpv6/option_iaaddress_test.go
+++ dhcpv6/option_iaaddress_test.go
@@ -3,6 +3,7 @@ package dhcpv6
 import (
 	"net"
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/require"
 )
@@ -17,8 +18,8 @@ func TestOptIAAddressParse(t *testing.T) {
 	opt, err := ParseOptIAAddress(data)
 	require.NoError(t, err)
 	require.Equal(t, net.IP(ipaddr), opt.IPv6Addr)
-	require.Equal(t, uint32(0x0a0b0c0d), opt.PreferredLifetime)
-	require.Equal(t, uint32(0x0e0f0102), opt.ValidLifetime)
+	require.Equal(t, 0x0a0b0c0d*time.Second, opt.PreferredLifetime)
+	require.Equal(t, 0x0e0f0102*time.Second, opt.ValidLifetime)
 }
 
 func TestOptIAAddressParseInvalidTooShort(t *testing.T) {
@@ -51,8 +52,8 @@ func TestOptIAAddressToBytes(t *testing.T) {
 	}...)
 	opt := OptIAAddress{
 		IPv6Addr:          net.IP(ipBytes),
-		PreferredLifetime: 0x0a0b0c0d,
-		ValidLifetime:     0x0e0f0102,
+		PreferredLifetime: 0x0a0b0c0d * time.Second,
+		ValidLifetime:     0x0e0f0102 * time.Second,
 		Options: []Option{
 			&OptElapsedTime{
 				ElapsedTime: 0xaabb,
@@ -65,8 +66,8 @@ func TestOptIAAddressToBytes(t *testing.T) {
 func TestOptIAAddressString(t *testing.T) {
 	ipaddr := []byte{0x24, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15}
 	data := append(ipaddr, []byte{
-		0xa, 0xb, 0xc, 0xd, // preferred lifetime
-		0xe, 0xf, 0x1, 0x2, // valid lifetime
+		0x00, 0x00, 0x00, 70, // preferred lifetime
+		0x00, 0x00, 0x00, 50, // valid lifetime
 		0, 8, 0, 2, 0xaa, 0xbb, // options
 	}...)
 	opt, err := ParseOptIAAddress(data)
@@ -80,12 +81,12 @@ func TestOptIAAddressString(t *testing.T) {
 	)
 	require.Contains(
 		t, str,
-		"preferredlifetime=168496141",
+		"preferredlifetime=1m10s",
 		"String() should return the preferredlifetime",
 	)
 	require.Contains(
 		t, str,
-		"validlifetime=235864322",
+		"validlifetime=50s",
 		"String() should return the validlifetime",
 	)
 }
--- dhcpv6/option_iaprefix_test.go
+++ dhcpv6/option_iaprefix_test.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"net"
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/require"
 )
@@ -20,10 +21,10 @@ func TestOptIAPrefix(t *testing.T) {
 	if err != nil {
 		t.Fatal(err)
 	}
-	if pl := opt.PreferredLifetime; pl != 0xaabbccdd {
+	if pl := opt.PreferredLifetime; pl != 0xaabbccdd*time.Second {
 		t.Fatalf("Invalid Preferred Lifetime. Expected 0xaabbccdd, got %v", pl)
 	}
-	if vl := opt.ValidLifetime; vl != 0xeeff0011 {
+	if vl := opt.ValidLifetime; vl != 0xeeff0011*time.Second {
 		t.Fatalf("Invalid Valid Lifetime. Expected 0xeeff0011, got %v", vl)
 	}
 	if pr := opt.PrefixLength(); pr != 36 {
@@ -43,8 +44,8 @@ func TestOptIAPrefixToBytes(t *testing.T) {
 		0, 8, 0, 2, 0xaa, 0xbb, // options
 	}
 	opt := OptIAPrefix{
-		PreferredLifetime: 0xaabbccdd,
-		ValidLifetime:     0xeeff0011,
+		PreferredLifetime: 0xaabbccdd * time.Second,
+		ValidLifetime:     0xeeff0011 * time.Second,
 		prefixLength:      36,
 		ipv6Prefix:        net.IPv6zero,
 	}
@@ -69,8 +70,8 @@ func TestOptIAPrefixParseInvalidTooShort(t *testing.T) {
 
 func TestOptIAPrefixString(t *testing.T) {
 	buf := []byte{
-		0xaa, 0xbb, 0xcc, 0xdd, // preferredLifetime
-		0xee, 0xff, 0x00, 0x11, // validLifetime
+		0x00, 0x00, 0x00, 60, // preferredLifetime
+		0x00, 0x00, 0x00, 50, // validLifetime
 		36,                                                         // prefixLength
 		0x20, 0x01, 0x0d, 0xb8, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, // ipv6Prefix
 	}
@@ -85,12 +86,12 @@ func TestOptIAPrefixString(t *testing.T) {
 	)
 	require.Contains(
 		t, str,
-		"preferredlifetime=2864434397",
+		"preferredlifetime=1m",
 		"String() should return the preferredlifetime",
 	)
 	require.Contains(
 		t, str,
-		"validlifetime=4009689105",
+		"validlifetime=50s",
 		"String() should return the validlifetime",
 	)
 }
