0444670a63335cc631222e99f59f4c7bfdff83bd
Valerio Santinelli <santinelli@altralogica.it>
2020-03-11 15:58:27 +0100

    Added support for a custom logger when instantiating the server4 or server6 object
    Signed-off-by: Valerio Santinelli <santinelli@altralogica.it>

--- dhcpv6/server6/server_test.go
+++ dhcpv6/server6/server_test.go
@@ -4,7 +4,9 @@ import (
 	"context"
 	"log"
 	"net"
+	"sync"
 	"testing"
+	"time"
 
 	"github.com/insomniacslk/dhcp/dhcpv6"
 	"github.com/insomniacslk/dhcp/dhcpv6/nclient6"
@@ -28,7 +30,7 @@ func (f unconnectedConn) ReadFrom(b []byte) (int, net.Addr, error) {
 
 // utility function to set up a client and a server instance and run it in
 // background. The caller needs to call Server.Close() once finished.
-func setUpClientAndServer(handler Handler) (*nclient6.Client, *Server) {
+func setUpClientAndServer(handler Handler, logger *customLogger) (*nclient6.Client, *Server) {
 	laddr := &net.UDPAddr{
 		IP:   net.ParseIP("::1"),
 		Port: 0,
@@ -37,6 +39,11 @@ func setUpClientAndServer(handler Handler) (*nclient6.Client, *Server) {
 	if err != nil {
 		panic(err)
 	}
+
+	if logger != nil {
+		s.logger = logger
+	}
+
 	go func() {
 		_ = s.Serve()
 	}()
@@ -53,6 +60,30 @@ func setUpClientAndServer(handler Handler) (*nclient6.Client, *Server) {
 	return c, s
 }
 
+type customLogger struct {
+	tb testing.TB
+	called bool
+	mux    sync.Mutex
+}
+
+func (s *customLogger) Printf(format string, v ...interface{}) {
+	s.mux.Lock()
+	s.called = true
+	s.mux.Unlock()
+	s.tb.Logf("===CustomLogger BEGIN===")
+	s.tb.Logf(format, v...)
+	s.tb.Logf("===CustomLogger END===")
+}
+
+func (s *customLogger) PrintMessage(prefix string, message *dhcpv6.Message) {
+	s.mux.Lock()
+	s.called = true
+	s.mux.Unlock()
+	s.tb.Logf("===CustomLogger BEGIN===")
+	s.tb.Logf("%s: %s", prefix, message)
+	s.tb.Logf("===CustomLogger END===")
+}
+
 func TestServer(t *testing.T) {
 	handler := func(conn net.PacketConn, peer net.Addr, m dhcpv6.DHCPv6) {
 		msg := m.(*dhcpv6.Message)
@@ -66,7 +97,7 @@ func TestServer(t *testing.T) {
 		}
 	}
 
-	c, s := setUpClientAndServer(handler)
+	c, s := setUpClientAndServer(handler, nil)
 	defer s.Close()
 
 	ifaces, err := interfaces.GetLoopbackInterfaces()
@@ -75,4 +106,108 @@ func TestServer(t *testing.T) {
 
 	_, err = c.Solicit(context.Background(), dhcpv6.WithRapidCommit)
 	require.NoError(t, err)
+}
+
+func TestCustomLoggerForServer(t *testing.T) {
+	handler := func(conn net.PacketConn, peer net.Addr, m dhcpv6.DHCPv6) {
+		msg := m.(*dhcpv6.Message)
+		adv, err := dhcpv6.NewAdvertiseFromSolicit(msg)
+		if err != nil {
+			log.Printf("NewAdvertiseFromSolicit failed: %v", err)
+			return
+		}
+		if _, err := conn.WriteTo(adv.ToBytes(), peer); err != nil {
+			log.Printf("Cannot reply to client: %v", err)
+		}
+	}
+
+	c, s := setUpClientAndServer(handler, &customLogger{
+		tb: t,
+	})
+	defer s.Close()
+
+	ifaces, err := interfaces.GetLoopbackInterfaces()
+	require.NoError(t, err)
+	require.NotEqual(t, 0, len(ifaces))
+
+	_, err = c.Solicit(context.Background(), dhcpv6.WithRapidCommit)
+	require.NoError(t, err)
+	go func() {
+		time.Sleep(time.Second * 5)
+		require.Equal(t, true, s.logger.(*customLogger).called)
+	}()
+}
+
+func TestServerInstantiationWithCustomLogger(t *testing.T) {
+	handler := func(conn net.PacketConn, peer net.Addr, m dhcpv6.DHCPv6) {
+		msg := m.(*dhcpv6.Message)
+		adv, err := dhcpv6.NewAdvertiseFromSolicit(msg)
+		if err != nil {
+			log.Printf("NewAdvertiseFromSolicit failed: %v", err)
+			return
+		}
+		if _, err := conn.WriteTo(adv.ToBytes(), peer); err != nil {
+			log.Printf("Cannot reply to client: %v", err)
+		}
+	}
+
+	laddr := &net.UDPAddr{
+		IP:   net.ParseIP("::1"),
+		Port: 0,
+	}
+	s, err := NewServer("", laddr, handler, WithLogger(&customLogger{
+		tb: t,
+	}))
+	if err != nil {
+		t.Fatal(err)
+	}
+	require.NotNil(t, s)
+}
+
+func TestServerInstantiationWithSummaryLogger(t *testing.T) {
+	handler := func(conn net.PacketConn, peer net.Addr, m dhcpv6.DHCPv6) {
+		msg := m.(*dhcpv6.Message)
+		adv, err := dhcpv6.NewAdvertiseFromSolicit(msg)
+		if err != nil {
+			log.Printf("NewAdvertiseFromSolicit failed: %v", err)
+			return
+		}
+		if _, err := conn.WriteTo(adv.ToBytes(), peer); err != nil {
+			log.Printf("Cannot reply to client: %v", err)
+		}
+	}
+
+	laddr := &net.UDPAddr{
+		IP:   net.ParseIP("::1"),
+		Port: 0,
+	}
+	s, err := NewServer("", laddr, handler, WithSummaryLogger())
+	if err != nil {
+		t.Fatal(err)
+	}
+	require.NotNil(t, s)
+}
+
+func TestServerInstantiationWithDebugLogger(t *testing.T) {
+	handler := func(conn net.PacketConn, peer net.Addr, m dhcpv6.DHCPv6) {
+		msg := m.(*dhcpv6.Message)
+		adv, err := dhcpv6.NewAdvertiseFromSolicit(msg)
+		if err != nil {
+			log.Printf("NewAdvertiseFromSolicit failed: %v", err)
+			return
+		}
+		if _, err := conn.WriteTo(adv.ToBytes(), peer); err != nil {
+			log.Printf("Cannot reply to client: %v", err)
+		}
+	}
+
+	laddr := &net.UDPAddr{
+		IP:   net.ParseIP("::1"),
+		Port: 0,
+	}
+	s, err := NewServer("", laddr, handler, WithDebugLogger())
+	if err != nil {
+		t.Fatal(err)
+	}
+	require.NotNil(t, s)
 }
