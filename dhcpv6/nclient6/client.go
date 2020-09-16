a0be48d916fffb667bbc621f178d5f8e0a753633
Chris Koch <chrisko@google.com>
2020-09-16 00:27:19 -0700

    docs: fix package comments
    Signed-off-by: Chris Koch <chrisko@google.com>

--- dhcpv6/nclient6/client.go
+++ dhcpv6/nclient6/client.go
@@ -2,6 +2,7 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
+// Package nclient6 is a minimum-functionality client for DHCPv6.
 package nclient6
 
 import (
