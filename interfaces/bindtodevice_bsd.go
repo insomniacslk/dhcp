d11e07c6523bdfd6c1c1cef0737d3096e2c6afcf
Pablo Mazzini <pmazzini@gmail.com>
2021-08-10 13:37:41 +0100

    [interfaces] bsd

--- interfaces/bindtodevice_bsd.go
+++ interfaces/bindtodevice_bsd.go
@@ -15,5 +15,5 @@ func BindToInterface(fd int, ifname string) error {
 	if err != nil {
 		return err
 	}
-	return syscall.SetsockoptInt(fd, syscall.IPPROTO_IP, syscall.IP_RECVIF, iface.Index)
+	return syscall.SetsockoptInt(fd, unix.IPPROTO_IP, unix.IP_RECVIF, iface.Index)
 }
