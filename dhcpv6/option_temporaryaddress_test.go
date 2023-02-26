667e3a9e71512220079a51820d57f9e20e9194db
Chris Koch <chrisko@google.com>
2023-02-25 20:35:17 -0800

    changes bla bla new uio
    Signed-off-by: Chris Koch <chrisko@google.com>

--- dhcpv6/option_temporaryaddress_test.go
+++ dhcpv6/option_temporaryaddress_test.go
@@ -85,7 +85,7 @@ func TestIATAParseAndGetter(t *testing.T) {
 		{
 			buf:  []byte{0, 4, 0, 1, 0},
 			want: nil,
-			err:  uio.ErrBufferTooShort,
+			err:  uio.ErrUnreadBytes,
 		},
 		{
 			buf: []byte{
@@ -94,7 +94,7 @@ func TestIATAParseAndGetter(t *testing.T) {
 				1, 0, 0, // IAID too short
 			},
 			want: nil,
-			err:  uio.ErrBufferTooShort,
+			err:  uio.ErrUnreadBytes,
 		},
 		{
 			buf: []byte{
