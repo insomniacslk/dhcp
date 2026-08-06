f5a45b158c4588f5fc0fd3faa7099c4271e8205c
Michael Pesa <michael.pesa@icloud.com>
2026-08-06 16:33:12 -0700

    netboot: fix swallowed errors in ConfigureInterface
    The deferred rt.Close() handler checked the wrong variable (err, the
    named return, instead of cerr). Any error returned from the function
    body was overwritten by the (almost always nil) rt.Close() result.
    Override the named return error only when it is nil.
    
    Signed-off-by: Michael Pesa <michael.pesa@icloud.com>

--- netboot/netconf.go
+++ netboot/netconf.go
@@ -187,7 +187,7 @@ func ConfigureInterface(ifname string, netconf *NetConf) (err error) {
 		return err
 	}
 	defer func() {
-		if cerr := rt.Close(); err != nil {
+		if cerr := rt.Close(); cerr != nil && err == nil {
 			err = cerr
 		}
 	}()
